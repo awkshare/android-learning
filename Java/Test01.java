434a2ce58fff2798818bf3b2ffc4e90e697df7f3
whoisboy <503134504@qq.com>
2014-11-24 00:03:54 +0800

    add
    add

---
+++ Java/Test01.java
@@ -0,0 +1,21 @@
+//这个例子是计算学习成绩的优良中差，并且对不合理数据进行判断
+public class Test01{
+	public static void main(String args []){
+		int score = 60;
+		if(score >85 && score <=100){
+			System.out.println("成绩为优");
+		}
+		else if(score > 75 && score <= 85){
+			System.out.println("成绩为良");
+		}
+		else if(score >= 60 && score <= 75){
+			System.out.println("成绩为中");
+		}
+		else if(score >= 0 && score < 60){
+			System.out.println("成绩为差");
+		}
+		else if(score > 100 || score < 0){
+			System.out.println("不是合理数据");
+		}
+	}
+}
