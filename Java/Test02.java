fc041e0a891becd1674973bdfd1955589ae111dd
whoisboy <503134504@qq.com>
2014-11-24 00:22:51 +0800

    add
    add

--- Java/Test02.java
+++ Java/Test02.java
@@ -1,4 +1,4 @@
-//这个例子是写石头剪刀布的
+//这个例子是写石头剪刀布的a石头，b剪刀，c布
 public class Test02{
 	public static void main(String args []){
 		char play1 = 'a';
@@ -7,10 +7,28 @@ public class Test02{
 			System.out.println('平');
 		}
 		else if(play1 == 'a' && play2 == 'b'){
-			System.out.println("play1赢了");
+			System.out.println("play1玩家赢了");
 		}
-		else if(play1 == 'a' && play2 == 'b'){
-			System.out.println("play2赢了");
+		else if(play1 == 'a' && play2 == 'c'){
+			System.out.println("play2玩家赢了");
+		}
+		else if(play1 == 'b' && play2 == 'a'){
+			System.out.println("play2玩家赢了");
+		}
+		else if(play1 == 'b' && play2 == 'b'){
+			System.out.println("平了");
+		}
+		else if(play1 == 'b' && play2 == 'c'){
+			System.out.println("play2玩家赢了");
+		}
+		else if(play1 == 'c' && play2 == 'a'){
+			System.out.println("play1玩家赢了");
+		}
+		else if(play1 == 'c' && play2 == 'b'){
+			System.out.println("play2玩家赢了");
+		}
+		else if(play1 == 'c' && play2 == 'c'){
+			System.out.println("平了");
 		}
 	}
 }
